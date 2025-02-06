7d08c6154a39d011e7a3c6b56550a34df35f36fb
Tajul Islam <tajul.islam@portonics.com>
2025-02-06 23:38:38 +0600

    feat(initialization): connected to database
    connected to mysql database with typeorm

---
+++ ormconfig.ts
@@ -0,0 +1,18 @@
+import { ConnectionOptions } from 'typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: ConnectionOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['src/entities/*.ts'],
+  synchronize: true,
+  logging: true,
+};
+
+export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import { App } from '@/app';
 import { ExampleRoute } from '@routes/example.route';
 import { ValidateEnv } from '@utils/validateEnv';
+import { createConnection } from 'typeorm';
+import config from "../ormconfig";
 
 ValidateEnv();
 
 const routes = [new ExampleRoute()];
 const app = new App(routes);
-
-app.listen();
+createConnection(config).then(()=>{
+    app.listen();
+})
