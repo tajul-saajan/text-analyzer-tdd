8ce855db5fe01ce855fcc329ee0c76ab57f80703
Tajul Islam <tajul.islam@portonics.com>
2025-02-07 18:52:58 +0600

    feat(text-crud): added failing test to have id with text and empty body to throw error

--- src/tests/text.test.ts
+++ src/tests/text.test.ts
@@ -5,12 +5,18 @@ import { TextRoute } from '@routes/text.route';
 describe('Text CRUD Operations', () => {
   let app: App;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     app = new App([new TextRoute()]);
   });
 
   it('it should give us 201 from texts create endpoint', async () => {
-    const response = await request(app.getServer()).post('/texts');
+    const response = await request(app.getServer()).post('/texts').send({ content: 'hello world' });
     expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('should throw error with empty body', async () => {
+    const response = await request(app.getServer()).post('/texts').send({});
+    expect(response.status).toBe(400);
   });
 });
