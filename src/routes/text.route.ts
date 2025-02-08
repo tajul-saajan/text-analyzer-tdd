19859cd82ae9a2c5f1b2dde77651a44c4803d76c
Tajul Islam <tajul.islam@portonics.com>
2025-02-08 22:55:12 +0600

    feat(text-crud): added auth middleware

--- src/routes/text.route.ts
+++ src/routes/text.route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { Routes } from '@interfaces/route.interface';
 import { TextController } from '@controllers/text.controller';
+import { authenticate } from '@/middlewares/auth.middleware';
 
 export class TextRoute implements Routes {
   public path = '/texts';
@@ -14,9 +15,9 @@ export class TextRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.textController.getTexts);
-    this.router.post(`${this.path}`, this.textController.createText);
-    this.router.put(`${this.path}/:id`, this.textController.updateText);
-    this.router.delete(`${this.path}/:id`, this.textController.deleteText);
+    this.router.get(`${this.path}`, authenticate, this.textController.getTexts);
+    this.router.post(`${this.path}`, authenticate, this.textController.createText);
+    this.router.put(`${this.path}/:id`, authenticate, this.textController.updateText);
+    this.router.delete(`${this.path}/:id`, authenticate, this.textController.deleteText);
   }
 }
