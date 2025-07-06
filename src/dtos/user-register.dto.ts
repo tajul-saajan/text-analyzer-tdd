c51bd1c0b5c099b3d5dcddff472424621cda3457
Tajul Islam <tajul.islam@wsd.com>
2025-07-06 16:49:31 +0600

    feat(security): :lock: add complex password logics

--- src/dtos/user-register.dto.ts
+++ src/dtos/user-register.dto.ts
@@ -1,6 +1,7 @@
 import { SanitizeHtml } from '@/decorators/sanitizeHtml.decorator';
-import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 
+const USER_PASSWORD_REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/;
 export class UserRegisterDto {
   @IsString()
   @IsNotEmpty({ message: 'name can not be empty' })
@@ -13,7 +14,10 @@ export class UserRegisterDto {
 
   @IsString()
   @IsNotEmpty({ message: 'password can not be empty' })
-  @Length(6)
   @SanitizeHtml()
+  @Length(8, 50)
+  @Matches(USER_PASSWORD_REGEX, {
+    message: 'Password must include uppercase, lowercase, and number',
+  })
   password: string;
 }
